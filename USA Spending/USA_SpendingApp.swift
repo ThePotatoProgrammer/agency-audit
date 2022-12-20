6578985e9a1fd06057aaab16b4b094e9eda3ff14
Joshua Baker <thepotatoprogrammer@gmail.com>
2022-12-19 19:42:35 -0500

    Initial Commit

---
+++ USA Spending/USA_SpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  USA_SpendingApp.swift
+//  USA Spending
+//
+//  Created by Joshua Baker on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct USA_SpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
